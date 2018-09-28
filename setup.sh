38d63c544ade734b856af1399a2942b509cad6fe
StephLin <stephen359595@gmail.com>
2018-09-28 20:04:36 +0800

    Add libboost-all-dev

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 sudo apt-get update
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+
+# for ubuntu 16.04 installing llvm-6.0
 sudo apt-add-repository "deb http://apt.llvm.org/$(lsb_release -cs)/ llvm-toolchain-$(lsb_release -cs)-6.0 main"
 sudo apt-get update
-sudo apt-get install -y vim git python-dev python3 cmake build-essential llvm-6.0 clang-6.0 libclang-6.0-dev 
+
+sudo apt-get install -y vim git python-dev python3 cmake build-essential llvm-6.0 clang-6.0 libclang-6.0-dev libboost-all-dev 
 
 cp ./.vimrc ~
 mkdir -p ~/.vim/bundle
@@ -12,11 +15,10 @@ git clone --recursive https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundl
 
 mkdir ~/.ycm_build
 cd ~/.ycm_build
-cmake -G "Unix Makefiles" -DUSE_SYSTEM_LIBCLANG=ON . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
+cmake -G "Unix Makefiles" -DUSE_SYSTEM_BOOST=ON -DUSE_SYSTEM_LIBCLANG=ON . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
 cmake --build . --target ycm_core --config Release
 rm -r ~/.ycm_build
 
-cd ~/.vim/bundle/YouCompleteMe
-python install.py --clang-completer
+python ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
 
 vim +PluginInstall +qall
