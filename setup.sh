27bf8fb5d76cca017892c87a2f763d543fa73a34
StephLin <stephen359595@gmail.com>
2018-09-28 13:11:32 +0800

    Fix url error

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y vim git python-dev python3 cmake build-essential
 cp ./.vimrc ~
 mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone --recursive [https://github.com/Valloric/YouCompleteMe.git](https://github.com/Valloric/YouCompleteMe.git) ~/.vim/bundle/YouCompleteMe
+git clone --recursive https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
 
 mkdir ~/.ycm_build
 cd ~/.ycm_build
